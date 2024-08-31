bb4d2e2cc86b55abe57836ff24590cde65888866
Mariam_Tarek <mariamelsarag44@gmail.com>
2024-08-31 18:48:48 +0300

    security

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const xss = require('xss-clean');
+const sanitize = require('express-mongo-sanitize');
+const limitRate = require('express-rate-limit');
+const helmet = require('helmet');
 
 const httpStatusText = require('./utils/httpStatusText');
 const globalErrorHandler = require('./controller/errorController');
@@ -12,7 +16,19 @@ app.use(cors());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// set security HTTP headers
+app.use(helmet());
+// senitize nosql injection
+app.use(sanitize());
+//xss
+app.use(xss());
+// limit request from same api
+const limiter = limitRate({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour!',
+});
+app.use('/api', limiter);
 // This will make form data available in req.body
 // for reading static file
 app.use(express.static(`${__dirname}/public`));
